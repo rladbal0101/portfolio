dcc043ca3c05918f46f45b3deb43812dbea80799
rladbal0101 <rladbal0101@naver.com>
2023-08-08 17:56:08 +0900

    2023-08-08 nav 수정, footer 추가
    - nav 수정
    - footer 내용추가

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 import styled from 'styled-components';
 
 const StyledHeader = styled.div`
-  width: 100%;
+  width: calc(100% - 40px);
   height: 65px;
   border-bottom: 1px solid #8da8d8;
   position: fixed;
@@ -14,8 +14,7 @@ const StyledHeader = styled.div`
 const HeaderInner = styled.div`
   height: 100%;
   display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
+  justify-content: space-between;
   align-items: center;
 
   .nav {
@@ -23,27 +22,34 @@ const HeaderInner = styled.div`
     
     li {
       font-size: 14px;
-      padding: 0 20px;
+      padding: 0 15px;
     }
 
     li + li {
       border-left: 1px solid #333;
     }
-
+    li:first-child {
+      padding-left: 0;
+    }
     li:last-child {
       padding-right: 0;
     }
   }
 
   @media screen and (max-width: 768px) {
+    flex-direction: column;
+    justify-content: space-evenly;
 
     .logo {
       font-size: 22px;
     }
 
-    .nav li {
-      font-size: 12px;
-      padding: 0 10px;
+    .nav {
+      
+      li {
+        font-size: 12px;
+        padding: 0 10px;
+      }
     }
   }
 `;
@@ -56,6 +62,10 @@ const Logo = styled.div`
   }
 `;
 
+const NavWrapper =styled.div`
+  /* width: 100%; */
+`;
+
 const StyledFooter = styled.div`
   width: 100%;
   height: 100px;
@@ -63,22 +73,47 @@ const StyledFooter = styled.div`
   border-top: 1px solid #c7c7c7;
 `;
 
+const FooterInner = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  p {
+    font-size: 12px;
+  }
+
+  ul {
+    display: flex;
+    text-align: left;
+
+    li + li {
+      margin-left: 10px;
+    }
+  }
+`;
+
 function Layout(props) {
+  const thisYear = new Date;
+  console.log();
+
   return (
     <>
       {/* 헤더 영역 */}
       <StyledHeader id='top'>
         <HeaderInner className='inner'>
-          <Logo className='cursor-pointer logo'>Yumi Kim</Logo>
-          <ul className='nav'>
-            <li className='cursor-pointer'><a href='#profile'>Profile</a></li>
-            <li className='cursor-pointer'><a href='#introduce'>Introduce</a></li>
-            <li className='cursor-pointer'><a href='#education'>Educations</a></li>
-            <li className='cursor-pointer'><a href='#work'>Works</a></li>
-            <li className='cursor-pointer'><a href='#project'>Project</a></li>
-            <li className='cursor-pointer'><a href='#other'>Others</a></li>
-            <li className='cursor-pointer'><a href='#skill'>Skills</a></li>
-          </ul>
+          <Logo className='cursor-pointer logo'><a href='#'>Yumi Kim</a></Logo>
+          <NavWrapper>
+            <ul className='nav'>
+              <li className='cursor-pointer'><a href='#profile'>Profile</a></li>
+              <li className='cursor-pointer'><a href='#introduce'>Introduce</a></li>
+              <li className='cursor-pointer'><a href='#education'>Education</a></li>
+              <li className='cursor-pointer'><a href='#work'>Work</a></li>
+              <li className='cursor-pointer'><a href='#project'>Project</a></li>
+              <li className='cursor-pointer'><a href='#other'>Others</a></li>
+              <li className='cursor-pointer'><a href='#skill'>Skills</a></li>
+            </ul>
+          </NavWrapper>
         </HeaderInner>
       </StyledHeader>
 
@@ -86,9 +121,21 @@ function Layout(props) {
 
       {/* 푸터 영역 */}
       <StyledFooter>
-        <ul>
-          <li></li>
-        </ul>
+        <FooterInner className='inner'>
+          <p>&copy;{thisYear.getFullYear()} by Yumi Kim.</p>
+          <ul>
+            <li>
+              <p>Phone</p>
+              <p>010-2990-5887</p>
+            </li>
+            <li>
+              <p>Email</p>
+              <p>kyumi16@gmail.com</p>
+            </li>
+            {/* <li>Phone : 010-2990-5887</li> */}
+            {/* <li>Email : kyumi16@gmail.com</li> */}
+          </ul>
+        </FooterInner>
       </StyledFooter>
     </>
   );
